436f0c1f0a026d23adbcad49b917143d3dc90b1e
OlegChern <OLChernyavsky@mail.ru>
2020-05-01 11:25:12 +0300

    Added Alignment evaluation

--- src/extraction/mtcnn_face_extraction.py
+++ src/extraction/mtcnn_face_extraction.py
@@ -1,6 +1,8 @@
 from mtcnn import MTCNN
 
 from src.extraction.face_align import align_face_eyes, align_face_eyes_nose, AlignType
+from timeit import default_timer
+
 import cv2
 import os
 
@@ -14,6 +16,7 @@ align_funcs = {
 def extract_faces_mtcnn(loader, save_path, align=AlignType.NONE):
     detector = MTCNN()
     save_path = save_path.replace("\\", "/")
+    alignment_time = 0
 
     for image, image_path in loader.next_image():
         faces = detector.detect_faces(image)
@@ -33,9 +36,15 @@ def extract_faces_mtcnn(loader, save_path, align=AlignType.NONE):
             face_image = image[y1:y2, x1:x2]
 
             if align is not AlignType.NONE:
+                start_time = default_timer()
                 image = align_funcs[align](image, face)
+                end_time = default_timer()
+
+                alignment_time += (end_time - start_time)
 
             face_name = f"{image_name}_face_{idx}.jpg"
             face_path = save_dir + "/" + face_name
 
             cv2.imwrite(face_path, face_image)
+
+    return alignment_time
--- src/test_system/evaluation.py
+++ src/test_system/evaluation.py
@@ -1,8 +1,11 @@
 from src.clustering.clustering import ImageClusteringUnit
 from src.clustering.clustering_utils import evaluate_metrics, optimal_params_grid_search
 from src.image_processing.image_utils import sort_images
+from src.image_processing.image_loader import ImageLoader
 from src.test_system.logging import get_default_logger
 from src.embedding.embeddings_creation import ImageEmbeddingsCreator
+from src.extraction.mtcnn_face_extraction import extract_faces_mtcnn
+from src.extraction.face_align import AlignType
 from timeit import default_timer
 
 import os
@@ -11,6 +14,27 @@ from enum import Enum, auto
 
 class Metric(Enum):
     PRECISION, RECALL, F1 = auto(), auto(), auto()
+
+
+align_names = {AlignType.NONE: "none",
+               AlignType.EYES_ONLY: "eyes",
+               AlignType.EYES_NOSE: "eyes_nose"}
+
+
+def evaluate_mtcnn_alignment(images_path, save_path, aligns, logger=None):
+    if logger is None:
+        logger = get_default_logger("Alignment")
+
+    loader = ImageLoader(images_path)
+    for align_type in aligns:
+
+        target_path = save_path + "/" + align_names[align_type]
+        if not os.path.exists(target_path):
+            os.mkdir(target_path)
+
+        work_time = extract_faces_mtcnn(loader, target_path, align_type)
+
+        logger.info(f"{align_names[align_type]} for the image set took {work_time}s")
 
 
 def evaluate_embeddings_creator(models, path_to_faces, logger=None):
