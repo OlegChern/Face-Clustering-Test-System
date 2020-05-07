face extraction/normalization fixed and refactored

src/extraction/align_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+LANDMARKS_PREDICTOR_PATH = "./models/dlib_landmarks_detector/shape_predictor_68_face_landmarks.dat"
+
 TEMPLATE = np.float32([
     (0.0792396913815, 0.339223741112), (0.0829219487236, 0.456955367943),
     (0.0967927109165, 0.575648016728), (0.122141515615, 0.691921601066),
@@ -45,8 +47,6 @@ OUTER_EYES_AND_NOSE = [36, 45, 33]
 LEFT_EYE = [37, 38, 39, 40, 41, 42]
 RIGHT_EYE = [43, 44, 45, 46, 47, 48]
 NOSE = [32, 33, 34, 35, 36]
-
-LANDMARKS_PREDICTOR_PATH = "./models/dlib_landmarks_detector/shape_predictor_68_face_landmarks.dat"
 
 
 def distance(a, b):

src/extraction/face_extraction.py
@@ -3,35 +3,37 @@ from mtcnn import MTCNN
 from src.extraction.align_utils import LANDMARKS_PREDICTOR_PATH, RIGHT_EYE, LEFT_EYE, NOSE
 
 from timeit import default_timer
+from abc import abstractmethod
 
 import cv2
 import os
 import dlib
+import numpy as np
 
 
 class FaceExtractor:
+    Name = "Default Name"
     NormalizationTime = 0
 
     def extract_faces(self, loader, save_path, aligner=None):
-        pass
-
-
-class FaceExtractorMTCNN(FaceExtractor):
-
-    def __init__(self):
         self.NormalizationTime = 0
-        self.Detector = MTCNN()
 
-    def extract_faces(self, loader, save_path, aligner=None):
-        self.NormalizationTime = 0
+        save_path = save_path + "/" + self.Name
+
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+
+        save_path = save_path + "/" + aligner.Name if aligner is not None else "No normalization"
+
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
 
         for image, image_path in loader.next_image():
-            faces = self.Detector.detect_faces(image)
+            faces = self.get_bounding_boxes(image)
 
             sep_pos = image_path.rfind("/") + 1
             dot_pos = image_path.rfind('.')
             image_name = image_path[sep_pos:dot_pos].replace("\\", "/")
-            save_dir = save_path + "/" + image_name
 
             for idx, face in enumerate(faces):
                 if aligner is not None:
@@ -46,6 +48,7 @@ class FaceExtractorMTCNN(FaceExtractor):
                     y2 = y1 + height
                     face_image = image[y1:y2, x1:x2]
 
+                save_dir = save_path + "/" + image_name
                 if not os.path.exists(save_dir):
                     os.mkdir(save_dir)
 
@@ -53,58 +56,54 @@ class FaceExtractorMTCNN(FaceExtractor):
                 face_path = save_dir + "/" + face_name
 
                 cv2.imwrite(face_path, face_image)
+
+    @abstractmethod
+    def get_bounding_boxes(self, image):
+        ...
+
+
+class FaceExtractorMTCNN(FaceExtractor):
+    Name = "MTCNN Face Extractor"
+
+    def __init__(self):
+        self.NormalizationTime = 0
+        self.Detector = MTCNN()
+
+    def get_bounding_boxes(self, image):
+        return self.Detector.detect_faces(image)
 
 
 class FaceExtractorDlib(FaceExtractor):
+    Name = "Dlib-based Face Extractor"
 
     def __init__(self, landmarks_predictor_path=LANDMARKS_PREDICTOR_PATH):
-        self.AlignmentTime = 0
+        self.NormalizationTime = 0
         self.Detector = dlib.get_frontal_face_detector()
         self.LandmarksPredictor = dlib.shape_predictor(landmarks_predictor_path)
 
-    def extract_faces(self, loader, save_path, aligner=None):
-        self.AlignmentTime = 0
+    def get_bounding_boxes(self, image):
+        faces = self.Detector(image, 1)
+        boxes = list()
 
-        for image, image_path in loader.next_image():
-            faces = self.Detector(image, 1)
+        for face in faces:
+            x1 = face.left() if face.left() > 0 else 0
+            y1 = face.top() if face.top() > 0 else 0
+            width = face.width()
+            height = face.height()
 
-            sep_pos = image_path.rfind("/") + 1
-            dot_pos = image_path.rfind('.')
-            image_name = image_path[sep_pos:dot_pos].replace("\\", "/")
-            save_dir = save_path + "/" + image_name
+            bounding_box = dlib.rectangle(x1, y1, x1 + width, y1 + height)
+            points = self.LandmarksPredictor(image, bounding_box)
+            points = list(map(lambda p: (p.x, p.y), points.parts()))
+            points = np.float32(points)
 
-            for idx, face in enumerate(faces):
-                x1 = face.left() if face.left() > 0 else 0
-                y1 = face.top() if face.top() > 0 else 0
-                width = face.width()
-                height = face.height()
+            box = dict()
+            box.update({"box": (x1, y1, width, height)})
+            box.update({"keypoints": {
+                "left_eye": points[LEFT_EYE].mean(axis=0).astype("int"),
+                "right_eye": points[RIGHT_EYE].mean(axis=0).astype("int"),
+                "nose": points[NOSE].mean(axis=0).astype("int")
+            }})
 
-                if aligner is not None:
-                    bounding_box = dlib.rectangle(x1, y1, x1 + width, y1 + height)
-                    points = self.LandmarksPredictor(image, bounding_box)
+            boxes.append(box)
 
-                    face = dict()
-                    face.update({"box": (x1, y1, width, height)})
-                    face.update({"keypoints": {
-                        "left_eye": points[LEFT_EYE].mean(axis=0).astype("int"),
-                        "right_eye": points[RIGHT_EYE].mean(axis=0).astype("int"),
-                        "nose": points[NOSE].mean(axis=0).astype("int")
-                    }})
-
-                    start_time = default_timer()
-                    face_image = aligner.normalize_face(image, face)
-                    end_time = default_timer()
-
-                    self.AlignmentTime += (end_time - start_time)
-                else:
-                    x2 = x1 + width
-                    y2 = y1 + height
-                    face_image = image[y1:y2, x1:x2]
-
-                if not os.path.exists(save_dir):
-                    os.mkdir(save_dir)
-
-                face_name = f"{image_name}_face_{idx}.jpg"
-                face_path = save_dir + "/" + face_name
-
-                cv2.imwrite(face_path, face_image)
+        return boxes

src/extraction/face_normalization.py
@@ -2,8 +2,6 @@ import cv2
 import dlib
 
 import numpy as np
-from PIL import Image
-from enum import Enum, auto
 
 from src.extraction.align_utils import distance, rotate_point, is_between, cosine_formula, LANDMARKS_PREDICTOR_PATH, \
     INNER_EYES_AND_BOTTOM_LIP, OUTER_EYES_AND_NOSE, MINMAX_TEMPLATE
@@ -11,14 +9,14 @@ from src.extraction.align_utils import distance, rotate_point, is_between, cosin
 
 # Abstract class for face normalizing
 class FaceNormalizer:
-    Name = "DefaultAlignerName"
+    Name = "Default Aligner Name"
 
     def normalize_face(self, image, face):
         pass
 
 
 class MappingAligner(FaceNormalizer):
-    Name = "Mapping_Aligner"
+    Name = "Dlib Mapping Aligner"
 
     def __init__(self, indices=OUTER_EYES_AND_NOSE, detector_path=LANDMARKS_PREDICTOR_PATH, target_size=(224, 224)):
         self.Predictor = dlib.shape_predictor(detector_path)
@@ -43,7 +41,7 @@ class MappingAligner(FaceNormalizer):
 
 
 class EyesNoseAligner(FaceNormalizer):
-    Name = "Eyes_Nose_Aligner"
+    Name = "Eyes Nose Aligner"
 
     def normalize_face(self, image, face):
         left_eye = face["keypoints"]["left_eye"]
@@ -72,7 +70,7 @@ class EyesNoseAligner(FaceNormalizer):
         else:
             angle = np.degrees(angle)
 
-        M = cv2.getRotationMatrix2D(nose, angle, 1)
+        M = cv2.getRotationMatrix2D((nose[0], nose[1]), angle, 1)
         image = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]), flags=cv2.INTER_CUBIC)
 
         image = image[y:y2, x:x2]
@@ -81,9 +79,9 @@ class EyesNoseAligner(FaceNormalizer):
 
 
 class EyesOnlyAligner(FaceNormalizer):
-    Name = "Eyes_Only_Aligner"
+    Name = "Eyes Only Aligner"
 
-    def __init__(self, left_eye=(0.25, 0.25)):
+    def __init__(self, left_eye=(0.3, 0.3)):
         self.LeftEyeDesiredLocation = left_eye
 
     def normalize_face(self, image, face):

src/main.py
@@ -1,10 +1,24 @@
+from src.clustering.algorithms.approximate_rank_order import cluster_app_rank_order
+from src.clustering.algorithms.rank_order import cluster_rank_order
+from src.clustering.algorithms.scikit_algorithms import cluster_mean_shift, cluster_dbscan, cluster_kmeans, \
+    cluster_affinity_propagation, cluster_spectral, cluster_agglomerative, cluster_optics
 from src.image_processing.image_loader import ImageLoader
 from src.clustering.clustering_utils import find_taxicab_distance, find_cosine_similarity, find_euclidean_distance
-from src.test_system.evaluation import evaluate_mtcnn_alignment
-from src.extraction.face_align import EyesNoseAligner, EyesOnlyAligner, MappingAligner
+from src.embedding.models_code.face_net import FaceNet
+from src.embedding.models_code.open_face import OpenFace
+from src.embedding.models_code.deep_face import DeepFace
+from src.embedding.models_code.vgg_face import FaceVGG
+from src.test_system.evaluation import evaluate_normalizers, evaluate_embeddings_creator, \
+    evaluate_clustering_algorithms
+from src.extraction.face_normalization import EyesNoseAligner, EyesOnlyAligner, MappingAligner, FaceCropperVGG
+from src.clustering.algorithms.algorithms import chinese_whisperers, cluster_threshold
+from src.extraction.face_extraction import FaceExtractorDlib, FaceExtractorMTCNN
 
-embedding_file = "./results/embeddings/embeddings.txt"
+import numpy as np
+
+embedding_file = "./results/embeddings/deepface_embeddings.txt"
 save_path = "./results/extraction/"
+load_path = "./results/extraction/Dlib_alignment_224"
 images_path = "./images"
 sorted_path = "./results/clustered"
 
@@ -18,54 +32,72 @@ deep_face_weigths = "./models/deep_face/VGGFace2_DeepFace_weights_val-0.9034.h5"
 def test_extraction_and_alignment():
     # logger = get_file_logger()
 
-    aligners = [EyesOnlyAligner(), EyesNoseAligner(), MappingAligner(), None]
-    evaluate_mtcnn_alignment(images_path, save_path, aligners)
+    extractors = list()
+    extractors.append(FaceExtractorMTCNN())
+    extractors.append(FaceExtractorDlib())
 
+    normalizers = list()
+    normalizers.append(EyesOnlyAligner())
+    normalizers.append(EyesNoseAligner())
+    normalizers.append(MappingAligner())
+    normalizers.append(FaceCropperVGG())
+
+    evaluate_normalizers(images_path, save_path, extractors, normalizers)
 
 test_extraction_and_alignment()
 
-# models = dict()
 
-# facenet_model = FaceNet(facenet_sandberg_model)
-facenet_file = "./results/embeddings/embeddings.txt"
-# models.update({facenet_model: facenet_file})
+# test_extraction_and_alignment()
 
-# openface_model = OpenFace(openface_weights)
-# openface_file = "./results/embeddings/embeddings.txt"
-# models_code.update({openface_model: openface_file})
-
-# vgg_model = FaceVGG(vgg_face_weights)
-# vgg_file = "./results/embeddings/embeddings.txt"
-# models_code.update({vgg_model: vgg_file})
+models = list()
 #
-
-# deepface_model = DeepFace(deep_face_weigths)
-# deepface_file = "./results/embeddings/embeddings.txt"
-# models.update({deepface_model: deepface_file})
+# facenet_file = "./results/embeddings/facenet_embeddings.txt"
+# facenet_model = (FaceNet, facenet_sandberg_model, facenet_file)
 #
-# evaluate_embeddings_creator(models, save_path)
+# models.append(facenet_model)
+#
+#
+#
+# extractor = FaceExtractorDlib()
+# loader = ImageLoader(images_path)
+# extractor.extract_faces(loader, load_path, align=True)
+
+# openface_file = "./results/embeddings/openface_embeddings.txt"
+# openface_model = (OpenFace, openface_weights, openface_file)
+# models.append(openface_model)
+
+# vgg_file = "./results/embeddings/vgg_embeddings.txt"
+# vgg_model = (FaceVGG, vgg_face_weights, vgg_file)
+# models.append(vgg_model)
+#
+#
+deepface_model = DeepFace(deep_face_weigths)
+deepface_file = "./results/embeddings/deepface_embeddings.txt"
+models.append((DeepFace, deep_face_weigths, deepface_file))
+#
+# evaluate_embeddings_creator(models, load_path)
 
 algorithms = dict()
 distances = [find_euclidean_distance, find_cosine_similarity, find_taxicab_distance]
 
-# app_rank_order_range = {"threshold": [[5]], "n_neighbors": [5]}
+# app_rank_order_range = {"threshold": np.arange(0, 200, 0.2), "n_neighbors": [5]}
 # algorithms.update({"Approximate Rank-Order": (cluster_app_rank_order, app_rank_order_range)})
 #
-# rank_order_range = {"threshold": np.arange(10, 15, 0.2), "k_neighbors": range(5, 11), "distance": ["euclidean", "manhattan"]}
+# rank_order_range = {"threshold": np.arange(0, 200, 0.2), "k_neighbors": range(5, 11), "distance": ["euclidean", "manhattan"]}
 # algorithms.update({"Rank-Order": (cluster_rank_order, rank_order_range)})
-
-
-# threshold_range = {"threshold": np.arange(0.1, 1, 0.0001), "distance": distances}
+#
+#
+# threshold_range = {"threshold": np.arange(0, 200, 0.1), "distance": distances}
 # algorithms.update({"Threshold Clustering": (cluster_threshold, threshold_range)})
-
-# chinese_whisperers_range = {"threshold": np.arange(0.1, 10, 0.1), "iterations": range(10, 11),
+#
+# chinese_whisperers_range = {"threshold": np.arange(0, 200, 0.1), "iterations": [10],
 #                             "distance": distances}
 # algorithms.update({"Chinese Whisperers": (chinese_whisperers, chinese_whisperers_range)})
+
+mean_shift_range = {"bandwidth": np.arange(0.5, 1.0, 0.00001)}
+algorithms.update({"Mean Shift": (cluster_mean_shift, mean_shift_range)})
 #
-# mean_shift_range = {"bandwidth": np.arange(0.1, 40, 0.1)}
-# algorithms.update({"Mean Shift": (cluster_mean_shift, mean_shift_range)})
-# #
-# dbscan_range = {"eps": range(1, 30), "min_samples": range(1, 5), "metric": distances}
+# dbscan_range = {"eps": range(1, 50), "min_samples": range(1, 8), "metric": distances}
 # algorithms.update({"DBSCAN": (cluster_dbscan, dbscan_range)})
 #
 # kmeans_range = {"n_clusters": range(1, 10), "random_state": range(50, 300, 10)}
@@ -82,5 +114,5 @@ distances = [find_euclidean_distance, find_cosine_similarity, find_taxicab_dista
 #
 # optics_range = {"min_samples": range(2, 10), "metric": distances}
 # algorithms.update({"OPTICS": (cluster_optics, optics_range)})
-#
+
 # evaluate_clustering_algorithms(algorithms, embedding_path=embedding_file, results_path=sorted_path)